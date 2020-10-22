f8f4fbe4067477d74305552dc2c35a0f05ab183c
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-10-22 15:34:07 -0400

    initial commit

--- API.Data/Person.cs
+++ API.Data/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +19,7 @@ namespace API.Data
         public int Age { get; set; }
         public string Occupation { get; set; }
         //Add Location
+        [ForeignKey(nameof(Place))]
+        public virtual Place Place { get; set; }
     }
 }
