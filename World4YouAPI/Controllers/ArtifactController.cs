9476451230cb39e83164d0ef7a9bf37540403a7e
JezzyDeves <juncrhepo@gmail.com>
2020-10-22 16:13:27 -0400

    Added ArtifactService and basic Get to controller

---
+++ World4YouAPI/Controllers/ArtifactController.cs
@@ -0,0 +1,29 @@
+﻿using API.Services.ArtifactServices;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace World4YouAPI.Controllers
+{
+    [Authorize]
+    public class ArtifactController : ApiController
+    {
+        private ArtifactService CreateArtifactService()
+        {
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var artifactService = new ArtifactService(userID);
+            return artifactService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            ArtifactService service = CreateArtifactService();
+            var artifacts = service.GetArtifacts();
+            return Ok(artifacts);
+        }
+    }
+}
