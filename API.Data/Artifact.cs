37a4ca08830783fbceeabd351e1ad05845c34837
JezzyDeves <juncrhepo@gmail.com>
2020-10-22 16:04:54 -0400

    Added Guid property

--- API.Data/Artifact.cs
+++ API.Data/Artifact.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,18 @@ namespace API.Data
         public int ID { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
         [Required]
         public string Power { get; set; }
+        [Required]
         public string Description { get; set; }
-        //Add Location
+
+        [Required]
+        public Guid OwnerID { get; set; }
+
+        [ForeignKey(nameof(Place))]
+        public int PlaceID { get; set; }
+        public virtual Place Place { get; set; }
     }
 }
